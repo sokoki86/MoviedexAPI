a60b87aa97f46cb99fa50b825fa249edc484cd9b
Jessica <jmbaskerville1217@gmail.com>
2020-12-02 12:43:22 -0500

    production changes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const MOVIEDEX = require('./moviedex.json')
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet());
 app.use(cors());
 
@@ -49,6 +50,16 @@ if(req.query.genre){
 res.json(results)
 })
 const PORT = 8000
+
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
